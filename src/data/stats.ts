58a20da67ca0ea7fb69acec9a6c1e443bd5e258f
Beatriz Machado <beatrizrolandmachado@gmail.com>
2026-04-19 15:09:19 -0300

    update stats

--- src/data/stats.ts
+++ src/data/stats.ts
@@ -1,8 +1,8 @@
 import type { Stat } from "types";
 
 export const stats: Stat[] = [
-  { label: "CYBERSEC", value: 75 },
+  { label: "CYBERSEC", value: 80 },
   { label: "FULL-STACK", value: 60 },
-  { label: "PYTHON", value: 90 },
-  { label: "COFFEE ☕", value: 100 },
+  { label: "LLM INTEGRATION", value: 70 },
+  { label: "DATA PROCESSING", value: 90 },
 ];
